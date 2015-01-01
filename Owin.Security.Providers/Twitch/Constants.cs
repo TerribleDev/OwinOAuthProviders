1d3591b25707aa364fd46efadcb1483f9fffd3bb
Tommy <parnell.tommy@hotmail.com>
2015-01-01 17:54:39 -0500

    twitch auth works

---
+++ Owin.Security.Providers/Twitch/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Owin.Security.Providers.Twitch
+{
+    internal static class Constants
+    {
+        public const string DefaultAuthenticationType = "Twitch";
+    }
+}
---
+++ Owin.Security.Providers/Twitch/TwitchAuthenticationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Owin.Security.Providers.Twitch
+{
+    public static class TwitchAuthenticationExtensions
+    {
+        public static IAppBuilder UseTwitchAuthentication(this IAppBuilder app,
+            TwitchAuthenticationOptions options)
+        {
+            if (app == null)
+                throw new ArgumentNullException("app");
+            if (options == null)
+                throw new ArgumentNullException("options");
+
+            app.Use(typeof(TwitchAuthenticationMiddleware), app, options);
+
+            return app;
+        }
+
+        public static IAppBuilder UseTwitchAuthentication(this IAppBuilder app, string clientId, string clientSecret)
+        {
+            return app.UseTwitchAuthentication(new TwitchAuthenticationOptions
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            });
+        }
+    }
+}
