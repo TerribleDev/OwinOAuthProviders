56c641132f4fbbf827eaba764212b4c5cd841e0c
Maxime Roussin-Bélanger <mroussin@hotmail.com>
2015-03-28 15:59:49 -0400

    Added wargaming openid login

---
+++ Owin.Security.Providers/Wargaming/WargamingAccountAuthenticationExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Owin;
+using System;
+
+namespace Owin.Security.Providers.Wargaming
+{
+    public static class WargamingAccountAuthenticationExtensions
+    {
+        public static IAppBuilder UseWargamingAccountAuthentication(this IAppBuilder app, WargamingAuthenticationOptions options)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException("app");
+            }
+            if (options == null)
+            {
+                throw new ArgumentNullException("options");
+            }
+
+            return app.Use(typeof(WargamingAuthenticationMiddleware), app, options);
+        }
+
+        /// <summary>
+        /// Authenticate users using Steam
+        /// </summary>
+        /// <param name="app">The <see cref="IAppBuilder"/> passed to the configuration method</param>
+        /// <param name="applicationKey">The wargaming application key</param>
+        /// <returns>The updated <see cref="IAppBuilder"/></returns>
+        public static IAppBuilder UseWargamingAccountAuthentication(this IAppBuilder app, string appId)
+        {
+            return UseWargamingAccountAuthentication(app, new WargamingAuthenticationOptions
+            {
+                ProviderDiscoveryUri = "https://na.wargaming.net/id/openid/",
+                Caption = "Wargaming",
+                AuthenticationType = "Wargaming",
+                CallbackPath = new PathString("/signin-wargaming"),
+                AppId = appId
+            });
+        }
+    }
+}
---
+++ Owin.Security.Providers/Wargaming/WargamingAuthenticationMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataHandler;
+using Microsoft.Owin.Security.DataProtection;
+using Microsoft.Owin.Security.Infrastructure;
+using Owin.Security.Providers.OpenID;
+
+namespace Owin.Security.Providers.Wargaming
+{
+    public class WargamingAuthenticationMiddleware : OpenIDAuthenticationMiddlewareBase<WargamingAuthenticationOptions>
+    {
+
+        /// <summary>
+        /// Initializes a <see cref="SteamAuthenticationMiddleware"/>
+        /// </summary>
+        /// <param name="next">The next middleware in the OWIN pipeline to invoke</param>
+        /// <param name="app">The OWIN application</param>
+        /// <param name="options">Configuration options for the middleware</param>
+        public WargamingAuthenticationMiddleware(OwinMiddleware next, IAppBuilder app, WargamingAuthenticationOptions options)
+            : base(next, app, options)
+        { }
+
+        protected override AuthenticationHandler<WargamingAuthenticationOptions> CreateSpecificHandler()
+        {
+            return new WargamingAuthenticationHandler(_httpClient, _logger);
+        }
+    }
+}
