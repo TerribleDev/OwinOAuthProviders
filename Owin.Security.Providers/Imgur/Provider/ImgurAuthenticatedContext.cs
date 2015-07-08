dd9c4be1c3944ce0b4b7d6ef2d4b407a67531328
Albireo <kappa7194@hotmail.it>
2015-07-08 23:39:16 +0200

    Implement imgur authenticated context

--- Owin.Security.Providers/Imgur/Provider/ImgurAuthenticatedContext.cs
+++ Owin.Security.Providers/Imgur/Provider/ImgurAuthenticatedContext.cs
@@ -1,6 +1,9 @@
 ﻿namespace Owin.Security.Providers.Imgur.Provider
 {
+    using System.Security.Claims;
+
     using Microsoft.Owin;
+    using Microsoft.Owin.Security;
     using Microsoft.Owin.Security.Provider;
 
     public class ImgurAuthenticatedContext : BaseContext<ImgurAuthenticationOptions>
@@ -8,5 +11,23 @@
         public ImgurAuthenticatedContext(IOwinContext context, ImgurAuthenticationOptions options) : base(context, options)
         {
         }
+
+        public string AccessToken { get; set; }
+
+        public int AccountId { get; set; }
+
+        public string AccountUsername { get; set; }
+
+        public int ExpiresIn { get; set; }
+
+        public ClaimsIdentity Identity { get; set; }
+
+        public AuthenticationProperties Properties { get; set; }
+
+        public string RefreshToken { get; set; }
+
+        public string Scope { get; set; }
+
+        public string TokenType { get; set; }
     }
 }
