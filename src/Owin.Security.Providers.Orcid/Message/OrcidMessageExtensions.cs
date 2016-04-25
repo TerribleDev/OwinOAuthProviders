939311e192a9b23f06852a2352875f9438ab68a4
Rick Zakharov <rick.zakharov@gmail.com>
2016-04-25 08:08:15 -0400

    Added ORCID authentication provider

---
+++ src/Owin.Security.Providers.Orcid/Message/OrcidMessageExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Owin.Security.Providers.Orcid.Message
+{
+    public static class OrcidMessageExtensions
+    {
+        public static OrcidAuthenticatedContext ToAuthenticationContext(this string json, IOwinContext context, string orcid, string accessToken)
+        {
+            var profile = JsonConvert.DeserializeObject<OrcidProfileMessage>(json);
+
+            var user = JObject.Parse(json);
+
+            var authenticatedContext = new OrcidAuthenticatedContext(context, user, accessToken);
+
+            var email = profile.OrcidProfile.OrcidBio.ContactDetails.Email.LastOrDefault();
+            if (email != null)
+                authenticatedContext.Email = email.Value;
+
+			authenticatedContext.Id = orcid;
+			authenticatedContext.UserName = orcid;
+
+            authenticatedContext.FirstName = profile.OrcidProfile.OrcidBio.PersonalDetails.GivenNames.Value;
+            authenticatedContext.LastName = profile.OrcidProfile.OrcidBio.PersonalDetails.FamilyName.Value;
+
+            return authenticatedContext;
+        }
+    }
+}
