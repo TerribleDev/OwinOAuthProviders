8fa3f6bdb6f4c846541f456d22e1900dcb1bbc22
papinto <pauloaapinto@hotmail.com>
2016-05-26 21:49:21 -0700

    retarget 4.5

--- src/Owin.Security.Providers.MyHeritage/Resources.Designer.cs
+++ src/Owin.Security.Providers.MyHeritage/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Owin.Security.Providers.MyHeritage {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    var temp = new global::System.Resources.ResourceManager("Owin.Security.Providers.MyHeritage.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Owin.Security.Providers.MyHeritage.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
