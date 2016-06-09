e4d504a0f469cb542c9a9a652612693294a73b5e
Dave Timmins <davetimmins@outlook.com>
2016-06-10 11:26:12 +1200

    Added refreshtoken and updated urls (#163)

--- src/Owin.Security.Providers.ArcGISOnline/Provider/ArcGISOnlineUser.cs
+++ src/Owin.Security.Providers.ArcGISOnline/Provider/ArcGISOnlineUser.cs
@@ -1,12 +1,7 @@
 ﻿namespace Owin.Security.Providers.ArcGISOnline.Provider
 {
     public class ArcGISOnlineUser
-    {
-        public User User { get; set; }
-    }
-
-    public class User
-    {
+    {          
         public string Username { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
