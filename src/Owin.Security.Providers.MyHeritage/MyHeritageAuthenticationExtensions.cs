45c98eb1f3ca8e24fd75d60404a57e0c786d34c0
Paulo Pinto <papinto@users.noreply.github.com>
2016-06-11 06:09:40 -0700

    Added MyHeritage provider (#164)
    * Added MyHeritage Provider
    
    Initial commit for a MyHeritage provider
    
    * Fixed signin case
    
    * retarget 4.5
    
    * Add MyHeritage to readme
    
    * removed test references from Demo project
    
    * fixed MyHeritage id token name

---
+++ src/Owin.Security.Providers.MyHeritage/MyHeritageAuthenticationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Owin.Security.Providers.MyHeritage
+{
+    public static class MyHeritageAuthenticationExtensions
+    {
+        public static IAppBuilder UseMyHeritageAuthentication(this IAppBuilder app,
+            MyHeritageAuthenticationOptions options)
+        {
+            if (app == null)
+                throw new ArgumentNullException(nameof(app));
+            if (options == null)
+                throw new ArgumentNullException(nameof(options));
+
+            app.Use(typeof(MyHeritageAuthenticationMiddleware), app, options);
+
+            return app;
+        }
+
+        public static IAppBuilder UseMyHeritageAuthentication(this IAppBuilder app, string clientId, string clientSecret)
+        {
+            return app.UseMyHeritageAuthentication(new MyHeritageAuthenticationOptions
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            });
+        }
+    }
+}
