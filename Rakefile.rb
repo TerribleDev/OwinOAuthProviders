d7947f010a57c7e4aefca3e4ab6e4b383458186d
Tommy Parnell <tparnell8@gmail.com>
2016-07-03 13:36:18 -0400

    Revert "if else yo"
    This reverts commit d761e6c90470ff7842f03f9659c9eb65c8e95252.

--- Rakefile.rb
+++ Rakefile.rb
@@ -65,15 +65,14 @@ desc 'publish nugets'
 task :nuspec_publish do
  if ENV['APPVEYOR_REPO_TAG'] != 'true'
   puts 'not publishing since APPVEYOR_REPO_TAG has not been set. Please add a github tag'
-  else
+  return nil
+ end
+
+
   PROJECTS.each{|dir|
     Dir.chdir(dir) do
       sh "#{NUGET_EXE} push #{FileList["*.nupkg"].first} -ApiKey #{NUGET_KEY}"
     end
   }
   sh "#{NUGET_EXE} push #{FileList["*.nupkg"].first} -ApiKey #{NUGET_KEY}"
- end
-
-
-  
 end
